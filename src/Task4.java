702d057419914973fb2b9c7aa23027dda64402db
ShipilovIvan10 <shipilov.ivan.stud@gmail.com>
2023-11-02 12:58:59 +0200

    Изменения

--- src/Task4.java
+++ src/Task4.java
@@ -21,26 +21,98 @@ public class Task4 {
         }
 
         //Выполнение задания
-        //Способ №1: Через класс stringBuiler
-        
-        StringBuilder strBuild = new StringBuilder(Integer.toString(numb));
-        strBuild.
+        //Способ №1: Через класс stringBuilder
 
-        //Способ №1: через арифм. операции
+        //Преобразуем число numb в строку методом toString() класса Integer
+        // и передадим эту строку в конструктор класса stringBuilder и получим объект этого класса
+        StringBuilder strBuilderOrigin = new StringBuilder(Integer.toString(numb));
+
+        //Создадим объект stringBuilder для записи результата изменения числа
+        //Строковая переменная str для конструктора
+        String str = "";
+        StringBuilder strBuilderResult = new StringBuilder(str);
+
+        //На первую позицию результата помещается последняя цифра
+        strBuilderResult.insert(0, strBuilderOrigin.charAt(5) );
+
+        //На 2ю позицию результата - предпоследняя
+        strBuilderResult.insert(1, strBuilderOrigin.charAt(4));
+
+        //На 3-ю позицию - 3й символ
+        strBuilderResult.insert(2, strBuilderOrigin.charAt(2));
+
+        //На 4-ю - четвёртый
+        strBuilderResult.insert(3, strBuilderOrigin.charAt(3));
+
+        //На 5ю позицию - 2й символ исходного числа
+        strBuilderResult.insert(4, strBuilderOrigin.charAt(1));
+
+        //На последнюю 6-ю - первый исходной
+        strBuilderResult.insert(5, strBuilderOrigin.charAt(0));
+
+        //Вывод результата
+        System.out.println("Результат: " + strBuilderResult);
+
+        //Очистим результат и выполним с помощью цикла
+        strBuilderResult.delete(0,6);
+
+        //Способ 1.2. Также, но с помощью цикла
+
+        //Переменная для номера цикла
+        int indOrigin;
+        for (int i = 0; i <= 5; i++){
+            //3 и 4 символ не меняем
+            if (i == 2 || i == 3) {
+                indOrigin = i;
+            } else { //Остальные инверсируем
+                indOrigin = 5 - i;
+            }
+
+            strBuilderResult.insert(i, strBuilderOrigin.charAt(indOrigin));
+        }
+
+        //Вывод результата
+        System.out.println("Результат: " + strBuilderResult);
+
+
+        //Способ №2: через арифм. операции
 
         //Переменнвя результат
         int result = 0;
 
         //Шаг цикла
-        int i = 0;
+        int j = 0;
+
+        //Степень 10 на которую нужно умножать получившуюся циферку
+        int decr;
 
         while (numb > 0){
 
-            System.out.println(numb%(Math.pow(10,i))*(Math.pow(10, i-1)) + " " + numb);
+            //3 и 4 цифру реверсировать не нужно
+            if (j==2 || j==3){
+                decr = j;
+            }
+            else {
+                decr = 5 - j;
+            }
 
-            result += (int) ( numb % ( Math.pow(10,i)) * (Math.pow(10, i-1) ) );
-            numb /= 10;
-            i++;
+            //Размер разряда
+            int numbRank = (int) Math.pow(10, decr);
+
+            //Размер разряда в итоговом числе
+            int numbRunkInv = (int) Math.pow(10, j);
+
+            //Отделяем циферку и умножаем её на нужный порядок разряда
+            int v = (numb / numbRank) * numbRunkInv;
+
+            //System.out.println( v + "   " + numb );
+
+            //Подсчитываем результат
+            result += v;
+            //Откидываем обработанную циферку
+            numb %=  numbRank;
+            //Шаг цикла
+            j++;
         }
 
        //Вывод
